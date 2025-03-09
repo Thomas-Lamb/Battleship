d2b556a73a511ba1b1ba99b31d39a49a6c55e4d4
Thomas <thomas.lamballais@epitech.eu>
2025-03-09 21:38:54 +0100

    Start

---
+++ cmake-build-debug/CMakeFiles/Battleship.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Battleship.exe"
+  "Battleship.exe.manifest"
+  "Battleship.pdb"
+  "CMakeFiles/Battleship.dir/main.cpp.obj"
+  "CMakeFiles/Battleship.dir/main.cpp.obj.d"
+  "libBattleship.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Battleship.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
